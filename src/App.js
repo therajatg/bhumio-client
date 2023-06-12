514cedd800d4ec58473f0236bb96ad7439eff1e4
Rajat Gupta <rajatgupta@Rajats-MacBook-Air.local>
2023-06-12 18:53:21 +0530

    delete, update, filter and export functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,165 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarExport,
+} from "@mui/x-data-grid";
+import axios from "axios";
+import { Alert, Button, Snackbar, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import { UpdateInventoryModal } from "./UpdateInventoryModal";
 
 function App() {
+  const [rows, setRows] = useState([]);
+  const [filteredRows, setFilteredRows] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [filterText, setFilterText] = useState("");
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    getGridData();
+  }, []);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const getGridData = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000");
+      setRows(res.data);
+      setFilteredRows(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const filterHandler = () => {
+    const trimedFilteredText = filterText.trim();
+    setFilteredRows(
+      rows.filter(
+        (row) =>
+          row["Part #"].includes(trimedFilteredText) ||
+          row["Alt.Part#"].includes(trimedFilteredText)
+      )
+    );
+  };
+
+  const deleteHandler = async (params) => {
+    try {
+      const res = await axios.delete(
+        `http://localhost:5000/${params.target.value}`
+      );
+      setRows(res.data);
+      setFilteredRows(res.data);
+      setOpen(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const CustomToolbar = () => {
+    return (
+      <GridToolbarContainer>
+        <GridToolbarExport />
+      </GridToolbarContainer>
+    );
+  };
+
+  const columns = [
+    { field: "id" },
+    { field: "Part #", headerName: "Part #", width: 100 },
+    { field: "Alt.Part#", headerName: "Alt.Part#" },
+    { field: "name", headerName: "Name" },
+    { field: "brand", headerName: "Brand", width: 100 },
+    { field: "model", headerName: "Model" },
+    { field: "engine", headerName: "Engine" },
+    { field: "car", headerName: "Car", width: 100 },
+    { field: "location A", headerName: "location A" },
+    { field: "LOCATION A STOCK", headerName: "LOCATION A STOCK", width: 150 },
+    { field: "LOCATION B", headerName: "LOCATION B", width: 100 },
+    { field: "LOC B STOCK", headerName: "LOC B STOCK", width: 120 },
+    { field: "unit", headerName: "Unit" },
+    { field: "rate", headerName: "Rate", width: 100 },
+    { field: "Value", headerName: "Value" },
+    { field: "remarks", headerName: "Remarks" },
+    {
+      field: "",
+      headerName: "",
+      renderCell: (params) => (
+        <Button value={params.id} variant="contained" onClick={deleteHandler}>
+          Delete
+        </Button>
+      ),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ m: 4 }}>
+      <Snackbar
+        open={open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          Successfully Deleted!
+        </Alert>
+      </Snackbar>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          mb: 4,
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <TextField
+            size="small"
+            label="Search"
+            value={filterText}
+            onChange={(e) => setFilterText(e.target.value)}
+            variant="outlined"
+            sx={{ mr: 1 }}
+          />
+          <Button variant="contained" onClick={filterHandler}>
+            Filter
+          </Button>
+        </Box>
+        <Button variant="contained" onClick={() => setModal(true)}>
+          Update Inventory
+        </Button>
+      </Box>
+      {rows?.length && (
+        <DataGrid
+          rows={filteredRows}
+          columns={columns}
+          slots={{
+            toolbar: CustomToolbar,
+          }}
+          initialState={{
+            columns: {
+              columnVisibilityModel: {
+                id: false,
+              },
+            },
+          }}
+        />
+      )}
+      {modal && (
+        <UpdateInventoryModal
+          modal={modal}
+          setModal={setModal}
+          filteredRows={filteredRows}
+          setFilteredRows={setFilteredRows}
+          columns={columns}
+        />
+      )}
+    </Box>
   );
 }
 
---
+++ src/UpdateInventoryModal.js
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Modal, Box, Typography, Button, Alert, Snackbar } from "@mui/material";
+import axios from "axios";
+
+export const UpdateInventoryModal = ({
+  modal,
+  setModal,
+  filteredRows,
+  setFilteredRows,
+  columns,
+}) => {
+  const [newColumns, setNewColumns] = useState([]);
+  let updatedRows = [];
+
+  useEffect(() => {
+    const requiredColumns = columns.reduce((acc, current) => {
+      switch (current?.field) {
+        case "id":
+          acc.push(current);
+          return acc;
+        case "Part #":
+          acc.push({
+            ...current,
+            headerName: "Part",
+          });
+          return acc;
+        case "Alt.Part#":
+          acc.push({
+            ...current,
+            headerName: "Alt_Part",
+          });
+          return acc;
+        case "LOCATION A STOCK":
+          acc.push({
+            ...current,
+            headerName: "LocA_Stock",
+            width: 150,
+            editable: true,
+            type: "number",
+          });
+          return acc;
+        case "LOC B STOCK":
+          acc.push({
+            ...current,
+            headerName: "LocB_Stock",
+            width: 150,
+            editable: true,
+            type: "number",
+          });
+          return acc;
+        default:
+          return acc;
+      }
+    }, []);
+    setNewColumns(requiredColumns);
+  }, []);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 600,
+
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    overflow: "hidden",
+    overflowY: "scroll",
+    borderRadius: 2,
+  };
+
+  const saveDataHandler = async () => {
+    try {
+      const res = await axios.put("http://localhost:5000", {
+        updatedRows,
+      });
+      setFilteredRows(res.data);
+      setModal(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Modal
+      open={modal}
+      onClose={() => setModal(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography
+          id="modal-modal-title"
+          variant="h6"
+          component="h2"
+          align="center"
+        >
+          Update Inventory
+        </Typography>
+        <div style={{ height: 500, width: "100%" }}>
+          <DataGrid
+            rows={filteredRows}
+            columns={newColumns}
+            processRowUpdate={(params) => {
+              updatedRows.push({
+                id: params.id,
+                "LOCATION A STOCK": params["LOCATION A STOCK"],
+                "LOC B STOCK": params["LOC B STOCK"],
+              });
+            }}
+            initialState={{
+              columns: {
+                columnVisibilityModel: {
+                  id: false,
+                },
+              },
+            }}
+          />
+        </div>
+        <Box
+          sx={{ display: "flex", justifyContent: "flex-end", mt: 2, mb: 0.5 }}
+        >
+          <Button variant="contained" sx={{ mr: 2 }} onClick={saveDataHandler}>
+            Save
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ mr: 1 }}
+            onClick={() => setModal(false)}
+          >
+            Close
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
+};
